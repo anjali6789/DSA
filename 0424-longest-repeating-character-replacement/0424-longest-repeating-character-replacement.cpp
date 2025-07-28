fb8accbbf1a5d740ad98c50fbe9adfb1438d0430
anjali6789 <78044831+anjali6789@users.noreply.github.com>
2025-07-28 22:53:34 +0530

    Time: 5 ms (53.59%), Space: 10.8 MB (69.9%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -7,8 +7,11 @@ public:
         int maxfrq=0;
         int ans=0;
         while(j<s.size()){
+            //incrementing the freq of current character
             maxFreq[s[j]-'A']++;
+            //calculating the max freq up till now
             maxfrq=max(maxfrq,maxFreq[s[j]-'A']);
+            //if it is not under k make it under k 
             while(j-i+1-maxfrq>k){
                 maxFreq[s[i]-'A']--;
                 i++;
@@ -16,7 +19,7 @@ public:
                 for(int i=0;i<26;i++){
                     maxfrq=max(maxfrq,maxFreq[i]);
                 }
-            }
+            }// if it is under k then this is the ans now increment j
                 ans=max(ans,j-i+1);
                 j++;
             
