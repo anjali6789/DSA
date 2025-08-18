a2728dd388279e9ab08d71f39d9ab0f48d12bed6
anjali6789 <78044831+anjali6789@users.noreply.github.com>
2025-08-19 01:19:57 +0530

    Time: 0 ms (100%), Space: 8.7 MB (25.49%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,15 +3,10 @@ public:
     bool isPalindrome(long long x) {
         if(x==0) return true;
         if(x<0||x%10==0) return false; // negatives cant be palindrome and numbers like 10 1000 cant be either
-        long long temp=x;
-        long long temp2=0;
-        while(temp!=0){
-            temp2=temp2+(temp%10);
-            cout<<temp2<<endl;
-            temp2=temp2*10;
-            temp=temp/10;
-        }if(temp2/10==x) {
-            return true;
-        } return false;
+        int reversed=0;
+        while(x>reversed){
+            reversed=reversed*10+(x%10);
+            x=x/10;
+        }return (reversed==x|| x == reversed / 10 );
     }
 };
