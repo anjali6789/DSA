797b0a63cfc6d2953c8e3e2b59bff01c6fd18c22
anjali6789 <78044831+anjali6789@users.noreply.github.com>
2025-08-19 21:29:37 +0530

    Time: 75 ms (71.18%), Space: 103.3 MB (82.78%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        int i = 0, j = 0;
-        int sum = 0, ans = 0;
-        unordered_set<int> st;
-
-        while (j < nums.size()) {
-            // if nums[j] already in set, shrink window
-            while (st.count(nums[j])) {
-                st.erase(nums[i]);
-                sum -= nums[i];
+        int i=0;
+        int j=0;
+        int sum=0;
+        int ans=0;
+        // mp to store freq of elements
+        unordered_map<int,int>mp;
+        while(j<nums.size()){
+            sum+=nums[j];
+            mp[nums[j]]++;
+            while(mp[nums[j]]>1){
+                //slide
+                mp[nums[i]]--;
+                sum-=nums[i];
                 i++;
-            }
-            st.insert(nums[j]);
-            sum += nums[j];
-            ans = max(ans, sum);
+            }ans=max(ans,sum);
             j++;
-        }
-        return ans;
+        }return ans;
     }
 };
