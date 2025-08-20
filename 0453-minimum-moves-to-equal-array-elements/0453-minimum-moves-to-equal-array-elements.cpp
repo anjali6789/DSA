b50696661c4a30b88a1de7dc5c12dd4aa12a8449
anjali6789 <78044831+anjali6789@users.noreply.github.com>
2025-08-21 01:06:15 +0530

    Time: 0 ms (100%), Space: 32.1 MB (26.16%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
+    // int minMoves(vector<int>& nums) {
+    //     int minimum=INT_MAX;
+    //     for(auto &it:nums){
+    //         minimum=min(minimum,it);
+    //     }
+    //     int sum=0;
+    //     for(auto &it:nums){
+    //         sum+=it-minimum;
+    //     }return sum;
+    // }
+    //making it one pass
     int minMoves(vector<int>& nums) {
         int minimum=INT_MAX;
-        for(auto &it:nums){
-            minimum=min(minimum,it);
-        }
         int sum=0;
         for(auto &it:nums){
-            sum+=it-minimum;
-        }return sum;
+            sum+=it;
+            minimum=min(minimum,it);
+        }
+        return sum-(long long)minimum*nums.size();
     }
 };
